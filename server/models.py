9023e27a208d9861288af56da0104e25a4431f73
lisa ballard <lballard.cat@gmail.com>
2013-09-28 16:23:52 -0700

    adds posting app

--- server/models.py
+++ server/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.forms import ModelForm
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=100)
-    school = models.CharField(max_length=255)
+class Reading(models.Model):
+    timestamp = models.IntegerField()
+    device_id = models.IntegerField()
+    trip_id = models.IntegerField()
+    sensor_id = models.CharField()
+    sensor_value = models.CharField()
--- server/views.py
+++ server/views.py
@@ -1,7 +1,15 @@
 class sensordata(View):
 
     def get(self, request):
-        return {}
+
+
 
     def post(self, request):
-        return {}
+        timestamp = request.GET.get('t', None)
+        device_id = request.GET.get('devID', None)
+        trip_id = request.GET.get('tripID', None)
+        sensor_id = request.GET.get('n1', None)
+        sensor_value = request.GET.get('v1', None)
+
+        reading = Reading(timestamp=timestamp, device_id=device_id, trip_id=trip_id, sensor_id=sensor_id, sensor_value=sensor_value)
+        reading.save()
