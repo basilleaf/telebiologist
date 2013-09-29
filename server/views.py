d75ad60d2aab9d4754591dcfd77095b210c818a6
lisa ballard <lballard.cat@gmail.com>
2013-09-28 17:43:37 -0700

    fixed oopsie

--- server/views.py
+++ server/views.py
@@ -15,11 +15,11 @@ class sensordata(View):
         return HttpResponse(jsondata, mimetype='application/json')
 
     def post(self, request):
-        timestamp = request.GET.get('t', None)
-        device_id = request.GET.get('devID', None)
-        trip_id = request.GET.get('tripID', None)
-        sensor_id = request.GET.get('n1', None)
-        sensor_value = request.GET.get('v1', None)
+        timestamp = request.POST.get('t', None)
+        device_id = request.POST.get('devID', None)
+        trip_id = request.POST.get('tripID', None)
+        sensor_id = request.POST.get('n1', None)
+        sensor_value = request.POST.get('v1', None)
 
         reading = Reading(timestamp=timestamp, device_id=device_id, trip_id=trip_id, sensor_id=sensor_id, sensor_value=sensor_value)
         reading.save()
