74a716e4e82cc9a79f355e9eb4ea1c8b22340d4e
lisa ballard <lballard.cat@gmail.com>
2013-09-28 21:37:48 -0700

    testing a view that has paging lol

--- server/views.py
+++ server/views.py
@@ -4,6 +4,7 @@ import json
 from django.core import serializers
 from server.models import *
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 class testview(View):
 
