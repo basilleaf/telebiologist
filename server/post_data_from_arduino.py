04422197b9fafe0fa8d5194abc97b6afd9e9e092
lisa ballard <lballard.cat@gmail.com>
2013-09-29 12:25:23 -0700

    added script for reading from arduino and posting to this thang

---
+++ server/post_data_from_arduino.py
@@ -0,0 +1,21 @@
+import serial
+import urllib
+import urllib2
+
+url = 'http://powerful-sands-3128.herokuapp.com/sciencehackday2014fromthefuture'
+
+ser = serial.Serial('/dev/tty.usbmodemfa131', 19200)
+while True:
+    data = ser.readline()
+    try:
+        (reading, time) = data.split(';')
+        (n1, v1) = reading.split(':')
+        v1 = int(v1)
+        t = int(time.split(':')[1])
+        tripID = 99999
+        devID = 99999
+        payload = {'t':t,  'n1': n1, 'v1': v1, 'tripID': tripID, 'devID': devID}
+        payload_encoded = urllib.urlencode(payload)
+        urllib2.urlopen(url, payload_encoded)
+    except:
+        pass
